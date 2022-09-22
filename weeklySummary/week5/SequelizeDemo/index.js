56bc4b5379b86a4f936f4755f4f8d342b2abc48c
Benjamin Odisho <Semiroundpizza8@gmail.com>
2022-09-21 19:14:26 -0500

    Updating with sequelize lab

---
+++ weeklySummary/week5/SequelizeDemo/index.js
@@ -0,0 +1,33 @@
+const {
+  Post, User
+} = require('./db');
+
+const express = require("express");
+const app = express();
+
+app.get("/", async (req, res, next) => {
+  const posts = await Post.findAll({
+    include: [
+      User
+    ]
+  });
+
+  res.send(`
+        <body>
+            ${posts
+      .map(post => `
+          <div>
+            <h2>${post.title}</h2>
+            <h3>${post.user.name}</h3>
+            <p>${post.content}</p>
+          </div>
+      `).join("")
+    }
+        </body>
+    `)
+})
+
+const PORT = 3000;
+app.listen(PORT, (test) => {
+  console.log(`Connected to: https://localhost:${PORT}`,)
+});
