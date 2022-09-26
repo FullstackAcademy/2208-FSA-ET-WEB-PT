0d73c61fdeb7ca8699a003d06428c07fc0b86ae9
Benjamin Odisho <Semiroundpizza8@gmail.com>
2022-09-26 17:17:07 -0500

    Removed things from week 6

--- weeklySummary/week6/NodeDBDemo/db/index.js
+++
@@ -1,6 +0,0 @@
-const pg = require("pg");
-const client = new pg.Client("postgres://localhost:5444/nodeIntroDB");
-// const client = pg.Client("postgresL//localhost:5432/nodeIntroDB");
-
-client.connect();
-module.exports = client;
